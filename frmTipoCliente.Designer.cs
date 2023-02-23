dd715245eadf314a3ad113b128452c26a870f80d
DATSUAM <calinfllowatsuamarrobo@gmail.com>
2023-02-22 20:43:30 -0500

    Inicio del proyecto

---
+++ frmTipoCliente.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace AdoNetDesconectado
+{
+    partial class frmTipoCliente
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmTipoCliente));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tbsNuevo = new System.Windows.Forms.ToolStripButton();
+            this.tsbEditar = new System.Windows.Forms.ToolStripButton();
+            this.tsbEliminar = new System.Windows.Forms.ToolStripButton();
+            this.tsbSalir = new System.Windows.Forms.ToolStripButton();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tbsNuevo,
+            this.tsbEditar,
+            this.tsbEliminar,
+            this.tsbSalir});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(423, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tbsNuevo
+            // 
+            this.tbsNuevo.Image = ((System.Drawing.Image)(resources.GetObject("tbsNuevo.Image")));
+            this.tbsNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tbsNuevo.Name = "tbsNuevo";
+            this.tbsNuevo.Size = new System.Drawing.Size(62, 22);
+            this.tbsNuevo.Text = "Nuevo";
+            this.tbsNuevo.Click += new System.EventHandler(this.nuevoRegistro);
+            // 
+            // tsbEditar
+            // 
+            this.tsbEditar.Image = ((System.Drawing.Image)(resources.GetObject("tsbEditar.Image")));
+            this.tsbEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbEditar.Name = "tsbEditar";
+            this.tsbEditar.Size = new System.Drawing.Size(57, 22);
+            this.tsbEditar.Text = "Editar";
+            this.tsbEditar.Click += new System.EventHandler(this.editarRegistro);
+            // 
+            // tsbEliminar
+            // 
+            this.tsbEliminar.Image = ((System.Drawing.Image)(resources.GetObject("tsbEliminar.Image")));
+            this.tsbEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbEliminar.Name = "tsbEliminar";
+            this.tsbEliminar.Size = new System.Drawing.Size(70, 22);
+            this.tsbEliminar.Text = "Eliminar";
+            // 
+            // tsbSalir
+            // 
+            this.tsbSalir.Image = ((System.Drawing.Image)(resources.GetObject("tsbSalir.Image")));
+            this.tsbSalir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsbSalir.Name = "tsbSalir";
+            this.tsbSalir.Size = new System.Drawing.Size(49, 22);
+            this.tsbSalir.Text = "Salir";
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.AllowUserToAddRows = false;
+            this.dgvDatos.AllowUserToDeleteRows = false;
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvDatos.Location = new System.Drawing.Point(0, 25);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.ReadOnly = true;
+            this.dgvDatos.Size = new System.Drawing.Size(423, 292);
+            this.dgvDatos.TabIndex = 1;
+            // 
+            // frmTipoCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(423, 317);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "frmTipoCliente";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Tipo Cliente";
+            this.Load += new System.EventHandler(this.cargarFormulario);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tbsNuevo;
+        private System.Windows.Forms.ToolStripButton tsbEditar;
+        private System.Windows.Forms.ToolStripButton tsbEliminar;
+        private System.Windows.Forms.ToolStripButton tsbSalir;
+        private System.Windows.Forms.DataGridView dgvDatos;
+    }
+}
+
---
+++ frmTipoCliente.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNetDesconectado
+{
+    public partial class frmTipoCliente : Form
+    {
+        string cadenaConexion = @"Server=localhost\SQLEXPRESS; DataBase=BancoBD; Integrated Security=true";
+        SqlDataAdapter adaptador;
+        SqlConnection conexion;
+        DataSet datos;
+        public frmTipoCliente()
+        {
+            InitializeComponent();
+
+            //Creamos la instancia de la Conexion
+            conexion = new SqlConnection(cadenaConexion);
+
+            // Creamos la instancia del Adaptador
+            adaptador = new SqlDataAdapter();
+
+            //Crear la instancia del DataSet
+            datos = new DataSet();
+
+            // Configuramos métodos del Adaptador
+            adaptador.SelectCommand = new SqlCommand("SELECT * FROM TipoCliente", conexion);
+        }
+
+        private void cargarFormulario(object sender, EventArgs e)
+        {
+            mostrarDatos();
+        }
+
+        private void mostrarDatos()
+        {
+            // Llenar Datos al DataSet (DataTable TipoCliente)
+            adaptador.Fill(datos, "TipoCliente");
+
+            //Enlazar datos al DataGidView
+            dgvDatos.DataSource = datos.Tables["TipoCliente"];
+        }
+
+        private void nuevoRegistro(object sender, EventArgs e)
+        {
+            var frm = new frmTipoClienteEdit();
+            if(frm.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Presionaste OK");
+            }
+        }
+
+        private void editarRegistro(object sender, EventArgs e)
+        {
+            var filaActual = dgvDatos.CurrentRow;
+            if (filaActual != null)
+            {
+                var ID = filaActual.Cells[0].Value.ToString();
+            }
+            DataRow fila = datos.Tables["TipoCliente"].Select($"ID={"ID"}").FirstOrDefault();
+            
+            var frm = new frmTipoClienteEdit();
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Presionaste OK");
+            }
+        }
+    }
+}
